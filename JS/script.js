7cc35fd0254ea8b9c2ba675d0f21cae3beb1fb48
Dayna-G <143474636+Dayna-G@users.noreply.github.com>
2023-11-01 13:40:55 -0700

    updated loop to display heights

--- JS/script.js
+++ JS/script.js
@@ -6,5 +6,10 @@ let pokemonList =[
 
 for(var i = 0; i < pokemonList.length; i++){
   document.write('<p>' + pokemonList[i].name + '</p>')
+  
+  if(pokemonList[i].height <10 && pokemonList[i].height >2  ){
+    document.write('<p>(height: ' + pokemonList[i].height + ') That is Big!</p>' );
+  } else if(pokemonList[i].height <3) {('<p> (height: ' + pokemonList[i].height + ' )</p>')
+}
 
 }
