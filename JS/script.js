c478a3029f4d63f038f642f8866d3dbc8726c265
Dayna-G <143474636+Dayna-G@users.noreply.github.com>
2023-11-29 00:48:48 -0800

    Wrapped code in IIFE

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,45 @@
 
-//let pokemonRepository = (function (pokemon){
+let pokemonRepository = (function (){
 let pokemonList =[ 
-{name:'Bulbasaur', height:'3', big: true , types:['grass','poison']},
-{name:'Charmander', height:'2', big: false , type:['fire']},
-{name:'Squirtle', height:'1', big: false, type:['water']},
-];
+{
+  name:'Bulbasaur', 
+  height: 3, 
+  big: true, 
+  types:['grass','poison']
+},
+{
+  name:'Charmander', 
+  height: 2, 
+  big: false , 
+  type:['fire']
+},
+{
+  name:'Squirtle', 
+  height: 1, 
+  big: false, 
+  type:['water']
+}
 
-pokemonList.forEach(function (pokemonRepository){
-let isBig = pokemon.big ? 'Wow that is big' : ' ';
-  document.write('<p>' + ' ' + pokemon.name + ' ' + pokemon.height+ ' ' + isBig + '</p>');
+]
 
-  function add(pokemon) {
+pokemonList.forEach(function (pokemon){
+let isBig = pokemon.big ? 'Wow that is big!' : ' ';
+  console.log(' ' + pokemon.name + ' ' + pokemon.height+ ' ' + isBig );
+  //return {
+ 
+  function getAll () {
+    return pokemonList;
+}
+  function add (pokemon) {
     pokemonList.push(pokemon);
   }
-
-  function getAll() {
-    return pokemonList;
-  }
- 
-  return {
-    add: add,
-    getAll: getAll
-  }
-});
-
-
-
-pokemonRepository.getAll();
+return {
+  getAll: getAll,
+  add: add
+}
+})
+}
+)();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:'Pikachu'});
+console.log(pokemonRepository.getAll());
