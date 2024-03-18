7394037b27a04e3cee91ccda28220c693e9e1ab1
Dayna-G <143474636+Dayna-G@users.noreply.github.com>
2024-03-17 19:29:51 -0700

    updated api link

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
   let pokemonRepository = (function () {
   let pokemonList = []; 
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=200";
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20";
   
 //Updated showModal
   function showModal (item) {
